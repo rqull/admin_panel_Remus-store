e832329c8c41e6b7a24157804b07b43cd10b249f
M.Rizqullah <mm.rizqullah@gmail.com>
2024-12-06 21:52:25 +0800

    Refactor MainScreen to include navigation for Buyers, Orders, Categories, Upload Banners, and Products. Updated sidebar routes to use screen IDs and set default selected screen to Vendors.

--- lib/views/main_screen.dart
+++ lib/views/main_screen.dart
@@ -1,3 +1,9 @@
+import 'package:app_web/views/side_bar_screens/buyers_screen.dart';
+import 'package:app_web/views/side_bar_screens/category_screen.dart';
+import 'package:app_web/views/side_bar_screens/orders_screen.dart';
+import 'package:app_web/views/side_bar_screens/product_screen.dart';
+import 'package:app_web/views/side_bar_screens/upload_banner_screen.dart';
+import 'package:app_web/views/side_bar_screens/vendors_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_admin_scaffold/admin_scaffold.dart';
@@ -10,6 +16,7 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  Widget _selectedScreen = VendorsScreen();
   @override
   Widget build(BuildContext context) {
     return AdminScaffold(
@@ -17,37 +24,37 @@ class _MainScreenState extends State<MainScreen> {
         backgroundColor: Colors.blue,
         title: Text('Managment'),
       ),
-      body: Text('Dashboard'),
+      body: _selectedScreen,
       sideBar: SideBar(
         items: [
           AdminMenuItem(
             title: 'Vendors',
-            route: '',
+            route: VendorsScreen.id,
             icon: CupertinoIcons.person_3,
           ),
           AdminMenuItem(
             title: 'Buyers',
-            route: '',
+            route: BuyersScreen.id,
             icon: CupertinoIcons.person,
           ),
           AdminMenuItem(
-            title: 'Categoris',
-            route: '',
-            icon: Icons.category,
-          ),
-          AdminMenuItem(
             title: 'Orders',
-            route: '',
+            route: OrdersScreen.id,
             icon: Icons.shopping_cart,
           ),
           AdminMenuItem(
+            title: 'Categoris',
+            route: CategoryScreen.id,
+            icon: Icons.category,
+          ),
+          AdminMenuItem(
             title: 'Upload Banners',
-            route: '',
+            route: UploadBannerScreen.id,
             icon: Icons.upload,
           ),
           AdminMenuItem(
             title: 'Upload Products',
-            route: '',
+            route: ProductScreen.id,
             icon: Icons.store,
           ),
         ],

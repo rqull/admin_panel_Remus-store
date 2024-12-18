b46da8bbe7219c197ec2564ea7043d311d583c51
M.Rizqullah <mm.rizqullah@gmail.com>
2024-12-18 11:41:27 +0800

    Enhance ProductScreen UI with size input and dynamic size list management. Added TextEditingController for size input, improved layout with spacing, and implemented functionality to add and remove sizes. Updated image upload section for better user experience.

--- lib/views/side_bar_screens/product_screen.dart
+++ lib/views/side_bar_screens/product_screen.dart
@@ -14,10 +14,17 @@ class ProductScreen extends StatefulWidget {
 
 class _ProductScreenState extends State<ProductScreen> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  final TextEditingController _sizeController = TextEditingController();
+
   final List<String> _categoriesList = [];
+
+  List<String> _sizesList = [];
   String? _selectedCategory;
 
-  List<Uint8List> images = <Uint8List>[];
+  bool _isEntered = false;
+
+  final List<Uint8List> images = <Uint8List>[];
 
   chooseImage() async {
     final pickedImages = await FilePicker.platform.pickFiles(
@@ -57,146 +64,199 @@ class _ProductScreenState extends State<ProductScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 400,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Product Information',
-            style: TextStyle(
-              fontSize: 19,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Enter product name',
-              filled: true,
-              fillColor: Colors.grey.shade200,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide.none,
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: 400,
+        child: Column(
+          spacing: 20,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Product Information',
+              style: TextStyle(
+                fontSize: 19,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            children: [
-              Flexible(
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Enter Price',
-                    filled: true,
-                    fillColor: Colors.grey.shade200,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide.none,
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Enter product name',
+                filled: true,
+                fillColor: Colors.grey.shade200,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            Row(
+              spacing: 20,
+              children: [
+                Flexible(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Enter Price',
+                      filled: true,
+                      fillColor: Colors.grey.shade200,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                width: 20,
-              ),
-              Flexible(
-                child: _buildDropDownField(),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Discount',
-              filled: true,
-              fillColor: Colors.grey.shade200,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide.none,
+                Flexible(
+                  child: _buildDropDownField(),
+                ),
+              ],
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Discount',
+                filled: true,
+                fillColor: Colors.grey.shade200,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Enter Description',
-              filled: true,
-              fillColor: Colors.grey.shade200,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide.none,
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Enter Description',
+                filled: true,
+                fillColor: Colors.grey.shade200,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Add Size',
-              filled: true,
-              fillColor: Colors.grey.shade200,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide.none,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          GridView.builder(
-            itemCount: images.length + 1,
-            shrinkWrap: true,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              mainAxisSpacing: 8,
-              crossAxisSpacing: 8,
-            ),
-            itemBuilder: (context, index) {
-              return index == 0
-                  ? Center(
-                      child: IconButton(
-                        onPressed: () {
-                          chooseImage();
-                        },
-                        icon: Icon(
-                          Icons.add,
+            Row(
+              spacing: 10,
+              children: [
+                Flexible(
+                  child: SizedBox(
+                    width: 200,
+                    child: TextFormField(
+                      controller: _sizeController,
+                      onChanged: (value) {
+                        setState(() {
+                          _isEntered = true;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Add Size',
+                        filled: true,
+                        fillColor: Colors.grey.shade200,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
                         ),
                       ),
-                    )
-                  : Image.memory(images[index - 1]);
-            },
-          ),
-          InkWell(
-            onTap: () {},
-            child: Container(
-                width: MediaQuery.of(context).size.width - 50,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(9),
-                ),
-                child: const Center(
-                  child: Text(
-                    'Upload Product',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                )),
-          )
-        ],
+                ),
+                _isEntered
+                    ? Flexible(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _sizesList.add(_sizeController.text);
+                              _sizeController.clear();
+                              _isEntered = false;
+                            });
+                          },
+                          child: Text('Add'),
+                        ),
+                      )
+                    : Text(''),
+              ],
+            ),
+            _sizesList.isNotEmpty
+                ? Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: SizedBox(
+                      height: 50,
+                      child: ListView.builder(
+                        itemCount: _sizesList.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return InkWell(
+                            onTap: () {
+                              setState(() {
+                                _sizesList.removeAt(index);
+                              });
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    _sizesList[index],
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  )
+                : Text(''),
+            GridView.builder(
+              itemCount: images.length + 1,
+              shrinkWrap: true,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                mainAxisSpacing: 8,
+                crossAxisSpacing: 8,
+              ),
+              itemBuilder: (context, index) {
+                return index == 0
+                    ? Center(
+                        child: IconButton(
+                          onPressed: () {
+                            chooseImage();
+                          },
+                          icon: Icon(
+                            Icons.add,
+                          ),
+                        ),
+                      )
+                    : Image.memory(images[index - 1]);
+              },
+            ),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                  width: MediaQuery.of(context).size.width - 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(9),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Upload Product',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )),
+            )
+          ],
+        ),
       ),
     );
   }
